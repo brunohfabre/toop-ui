8293d4e17efca4898746c766ad1d78b55fb4c623
Bruno Fabre <bruno.hfabre@gmail.com>
2024-07-10 15:23:56 -0300

    add theme provider

---
+++ src/pages/home.tsx
@@ -0,0 +1,33 @@
+import { useTheme } from '../components/theme-provider'
+
+export function Home() {
+  const { setTheme } = useTheme()
+
+  function handleChangeThemeToLight() {
+    setTheme('light')
+  }
+
+  function handleChangeThemeToDark() {
+    setTheme('dark')
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center gap-2 bg-gray-1 p-4 text-gray-12 antialiased transition-colors">
+      <button
+        type="button"
+        className="h-9 bg-gray-12 px-4 text-sm text-gray-1"
+        onClick={handleChangeThemeToLight}
+      >
+        Light theme
+      </button>
+
+      <button
+        type="button"
+        className="h-9 bg-gray-12 px-4 text-sm text-gray-1 dark:bg-red-500"
+        onClick={handleChangeThemeToDark}
+      >
+        Dark theme
+      </button>
+    </div>
+  )
+}
