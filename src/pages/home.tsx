a8acd4f1fe12176dadbf84a34028bd136cb63371
Bruno Fabre <bruno.hfabre@gmail.com>
2024-07-11 08:21:49 -0300

    add primary colors

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,38 +12,44 @@ export function Home() {
   }
 
   return (
-    <div className="flex h-screen items-center justify-center gap-2 bg-gray-1 p-4 text-gray-12 antialiased transition-colors">
-      <button
-        type="button"
-        className="h-9 rounded-md bg-gray-12 px-4 text-sm text-gray-1"
-        onClick={handleChangeThemeToLight}
-      >
-        Light theme
-      </button>
+    <div className="flex h-screen flex-col items-center justify-center gap-8 bg-gray-1 p-4 text-gray-12 antialiased transition-colors">
+      <div className="flex">
+        <div className="size-32 bg-primary-9" />
+      </div>
 
-      <button
-        type="button"
-        className="h-9 bg-gray-12 px-4 text-sm text-gray-1 dark:bg-red-500"
-        onClick={handleChangeThemeToDark}
-      >
-        Dark theme
-      </button>
+      <div className="flex gap-2">
+        <button
+          type="button"
+          className="h-9 rounded-md bg-gray-12 px-4 text-sm text-gray-1"
+          onClick={handleChangeThemeToLight}
+        >
+          Light theme
+        </button>
 
-      <button
-        type="button"
-        className="h-9 bg-gray-12 px-4 text-sm text-gray-1 dark:bg-red-500"
-        onClick={() => changeColor('crimson')}
-      >
-        Crimson
-      </button>
+        <button
+          type="button"
+          className="h-9 bg-gray-12 px-4 text-sm text-gray-1"
+          onClick={handleChangeThemeToDark}
+        >
+          Dark theme
+        </button>
 
-      <button
-        type="button"
-        className="h-9 bg-gray-12 px-4 text-sm text-gray-1 dark:bg-red-500"
-        onClick={() => changeColor('indigo')}
-      >
-        Indigo
-      </button>
+        <button
+          type="button"
+          className="h-9 bg-gray-12 px-4 text-sm text-gray-1"
+          onClick={() => changeColor('crimson')}
+        >
+          Crimson
+        </button>
+
+        <button
+          type="button"
+          className="h-9 bg-gray-12 px-4 text-sm text-gray-1"
+          onClick={() => changeColor('indigo')}
+        >
+          Indigo
+        </button>
+      </div>
     </div>
   )
 }
